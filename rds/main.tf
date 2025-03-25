d9106457b2f88a167ad66af6728b294dd6be15df
이희수 <hslee9234@naver.com>
2025-03-25 14:10:04 +0900

    fix: db name 오타 수정

--- rds/main.tf
+++ rds/main.tf
@@ -69,7 +69,7 @@ resource "aws_db_instance" "rds" {
   allocated_storage      = 20
   username               = var.db_username
   password               = var.db_password
-  db_name                = "jeff-db"
+  db_name                = "jeffDB"
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot    = true
